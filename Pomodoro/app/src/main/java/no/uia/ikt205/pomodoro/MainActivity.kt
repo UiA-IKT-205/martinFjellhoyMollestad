c8da3e7b6a81d1c5a6da03402d0f4ec9a66adb32
Martin Fjellhøy Mollestad <martfm13@student.uia.no>
2021-02-02 22:18:21 +0100

    changed durationButtons to SeekBar and added an option for repetitions

--- Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -1,88 +1,139 @@
 package no.uia.ikt205.pomodoro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
+import no.uia.ikt205.pomodoro.util.minutesToMilliSeconds
+import no.uia.ikt205.pomodoro.util.secondsToMilliSeconds
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var timer:CountDownTimer
     lateinit var startButton:Button
-    lateinit var coutdownDisplay:TextView
+    lateinit var changeDurationSeekBar:SeekBar
+    lateinit var editRepetitionsTextInput:EditText
 
-    var timeToCountDownInMs = 5000L
+    lateinit var countdownDisplay:TextView
+    lateinit var countdownText:TextView
+
+    var timeToCountDownInMs = 900000L //15 minutes
+    var timeToCountDownInMinutes=15L // a variable to store the chosen duration for next repetition
     val timeTicks = 1000L
-    val oneMinutinTicks = 60000L;
+
+    var numberOfRepetitions =0 //default 0 repetitions
+    val timeForBreakInMs = 900000L //15 Minutes
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
+        startButton = findViewById<Button>(R.id.startCountdownButton)
+        startButton.setOnClickListener(){
            startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
-
-
-       var thirtyMinutesButton = findViewById<Button>(R.id.setTimeDurationTo30MinutesBt)
-        thirtyMinutesButton.setOnClickListener(){
-            if(startButton.isEnabled){
-                setCountDownTime(oneMinutinTicks*30)
-            }
-        }
-        var sixtyMinutesButton = findViewById<Button>(R.id.setTimeDurationTo60MinutesBt)
-        sixtyMinutesButton.setOnClickListener(){
-            if(startButton.isEnabled){
-                setCountDownTime(oneMinutinTicks*60)
-            }
-        }
-        var ninetyMinutesButton = findViewById<Button>(R.id.setTimeDurationTo90MinutesBt)
-        ninetyMinutesButton.setOnClickListener(){
-            if(startButton.isEnabled){
-                setCountDownTime(oneMinutinTicks*90)
-            }
-        }
-        var oneTwentyMinutesButton = findViewById<Button>(R.id.setTimeDurationTo120MinutesBt)
-        oneTwentyMinutesButton.setOnClickListener(){
-            if(startButton.isEnabled){
-                setCountDownTime(oneMinutinTicks*120)
-            }
         }
 
+        countdownDisplay = findViewById<TextView>(R.id.countDownView)
+        countdownDisplay.setText(millisecondsToDescriptiveTime(minutesToMilliSeconds(15)))
+
+        countdownText = findViewById(R.id.countdownExplenationText)
+
+
+        editRepetitionsTextInput = findViewById<EditText>(R.id.numberOfRepetitionsEditText)
 
 
 
-    }
+        changeDurationSeekBar = findViewById<SeekBar>(R.id.changeTimerseekBar)
+        changeDurationSeekBar.max = 300
+        changeDurationSeekBar.min = 15
+        print("updated min/max values")
 
-    fun setCountDownTime(newCountDownTimeInMs:Long){
-        timeToCountDownInMs = newCountDownTimeInMs
-        updateCountDownDisplay(timeToCountDownInMs)
+        changeDurationSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                timeToCountDownInMs = minutesToMilliSeconds(i.toLong())
+                timeToCountDownInMinutes=i.toLong()
+                updateCountDownDisplay(timeToCountDownInMs)
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
     }
 
     fun startCountDown(v: View){
+        numberOfRepetitions=editRepetitionsTextInput.text.toString().toInt()
 
-        timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
+        countdownText.setText("Number of Repetitions left: $numberOfRepetitions")
+
+        //disables all inputs
+        startButton.isEnabled = false
+        editRepetitionsTextInput.isEnabled = false
+        changeDurationSeekBar.isEnabled = false
+
+        if(numberOfRepetitions>=0){
+
+            timer = object : CountDownTimer(timeToCountDownInMs, timeTicks) {
+
+                override fun onFinish() {
+                    if(numberOfRepetitions==0){
+                        Toast.makeText(this@MainActivity, "Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+
+                        //enables all inputs
+                        startButton.isEnabled=true
+                        editRepetitionsTextInput.isEnabled = true
+                        changeDurationSeekBar.isEnabled = true
+
+                    }else{
+                        startTimeForBreakCountDown(v)
+                        numberOfRepetitions--
+                    }
+
+
+                }
+
+                override fun onTick(millisUntilFinished: Long) {
+                    updateCountDownDisplay(millisUntilFinished)
+                }
+            }
+
+            timer.start()
+            Toast.makeText(this@MainActivity, "Remaining repetitions: $numberOfRepetitions", Toast.LENGTH_SHORT).show()
+
+        }else{
+            Toast.makeText(this@MainActivity, "Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
+            startButton.isEnabled=true
+        }
+
+
+    }
+    fun startTimeForBreakCountDown(v: View){
+        countdownText.setText("Pause for 15 min. $numberOfRepetitions repetitions left")
+
+
+        timer = object : CountDownTimer(timeForBreakInMs, timeTicks) {
+
             override fun onFinish() {
-                Toast.makeText(this@MainActivity,"Arbeidsøkt er ferdig", Toast.LENGTH_SHORT).show()
-                v.isEnabled=true
+                startCountDown(v)
             }
 
             override fun onTick(millisUntilFinished: Long) {
-               updateCountDownDisplay(millisUntilFinished)
+                updateCountDownDisplay(millisUntilFinished)
             }
         }
-        v.isEnabled=false
+
         timer.start()
     }
 
-    fun updateCountDownDisplay(timeInMs:Long){
-        coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
+
+
+    fun updateCountDownDisplay(timeInMs: Long){
+        countdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
     }
 
 }
